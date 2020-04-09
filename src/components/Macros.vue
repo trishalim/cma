0e8de4c8e006d0ec617bc99f25b5dd547e745584
Trisha Lim <limtrishajean@gmail.com>
2020-04-09 17:24:47 +0700

    don't use images for dots in Macros

--- src/components/Macros.vue
+++ src/components/Macros.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="macros">
     <div class="macro" v-if="carbs >= 0">
-      <img src="../assets/dot-red.svg" />{{ carbs }}g
+      <span class="dot dot-red"></span>{{ carbs }}g
     </div>
     <div class="macro" v-if="protein >= 0">
-      <img src="../assets/dot-blue.svg" />{{ protein }}g
+      <span class="dot dot-blue"></span>{{ protein }}g
     </div>
     <div class="macro" v-if="fats >= 0">
-      <img src="../assets/dot-yellow.svg" />{{ fats }}g
+      <span class="dot dot-orange"></span>{{ fats }}g
     </div>
   </div>
 </template>
@@ -38,5 +38,25 @@ export default {
 }
 .macro img {
   margin-right: 4px;
+}
+.dot {
+  display: inline-block;
+  width: 12px;
+  height: 10px;
+}
+.dot::after {
+  content: '•';
+  position: absolute;
+  font-size: 24px;
+  line-height: 10px;
+}
+.dot-red {
+  color: #f94642;
+}
+.dot-blue {
+  color: #3177bb; 
+}
+.dot-orange {
+  color: #fda120;
 }
 </style>
